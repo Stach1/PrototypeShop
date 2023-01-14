438911451e0a7fead57f517b2f14c9772127cb0d
Stach1 <stach1gam3r@gmail.com>
2023-01-14 14:55:35 -0300

    8 directional animation movement
    Player can now animate while walking, and stops at the right idle too

--- Assets/Scripts/Player/PlayerMov.cs
+++ Assets/Scripts/Player/PlayerMov.cs
@@ -9,14 +9,38 @@ public class PlayerMov : MonoBehaviour
     private float _movSpeed;
 
     private Rigidbody2D _rb;
+    private Animator _anim;
+
+    // The vector for the inputs made by the player
     private Vector2 _movInput;
+    // Saves last input before stopped moving (for animation)
+    private Vector2 _lastMovInput;
+
+    // If player not moving (for animations)
+    private bool _isIdle;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
     }
 
     private void Update()
+    {
+        MoveInput();
+        Animate();
+        
+    }
+
+    private void FixedUpdate()
+    {
+        // Character movement
+        _rb.velocity = _movInput * _movSpeed;
+
+        if (_isIdle) _rb.velocity = Vector2.zero;
+    }
+
+    private void MoveInput()
     {
         // Player's input
         float dirX = Input.GetAxisRaw("Horizontal");
@@ -24,13 +48,22 @@ public class PlayerMov : MonoBehaviour
 
         // Normalize input values for any controller
         _movInput = new Vector2(dirX, dirY).normalized;
+
+        // If there's no input then player is idle
+        _isIdle = dirX == 0 && dirY == 0;
     }
 
-    private void FixedUpdate()
+    void Animate()
     {
-        // Character movement
-        _rb.velocity = _movInput * _movSpeed;
+        // Takes the player input to choose the animation for the blend tree
+        // I used last mov input so when you stop moving it'll choose the right float
+        // for the idle animation
+        // Example: If you were moving up, the character will choose the idle up animation
+        _anim.SetFloat("InputX", _lastMovInput.x);
+        _anim.SetFloat("InputY", _lastMovInput.y);
+
+        // If you're idle then animator changes to idle blend tree, otherwise player will keep moving
+        // Last mov input gets saved here
+        if (_isIdle) _anim.SetBool("isMoving", false); else { _anim.SetBool("isMoving", true); _lastMovInput = _movInput; }
     }
-
-
 }
